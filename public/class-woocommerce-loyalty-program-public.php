68a6f707c17ffa087bbe1338ac35e3a4557c7210
Роман Ченский <romaleg.sky@yandex.ru>
2023-05-15 20:31:47 +0300

    added

--- public/class-woocommerce-loyalty-program-public.php
+++ public/class-woocommerce-loyalty-program-public.php
@@ -46,12 +46,13 @@ class Woocommerce_Loyalty_Program_Public {
 	 * @param      string    $woocommerce_loyalty_program       The name of the plugin.
 	 * @param      string    $version    The version of this plugin.
 	 */
+	public $loyalty_program_post_type;
 	public function __construct( $woocommerce_loyalty_program, $version ) {
-
+		$this->loyalty_program_post_type = 'loyalty_program';
 		$this->woocommerce_loyalty_program = $woocommerce_loyalty_program;
 		$this->version = $version;
-
-		add_action( 'woocommerce_register_form', 'add_custom_fields_to_registration_form' );
+		add_action( 'woocommerce_register_form', [ $this, 'add_date_fields_to_registration_form' ] );
+		add_filter( 'woocommerce_created_customer', [ $this, 'save_gate_fields_on_registration' ], 10, 1 );
 	}
 
 	/**
@@ -101,33 +102,90 @@ class Woocommerce_Loyalty_Program_Public {
 	}
 
 
-	public function add_custom_fields_to_registration_form() {
-		$custom_post_type = 'loyalty_program'; // Замените на фактический идентификатор вашего кастомного типа записей
-		
+	public function add_date_fields_to_registration_form() {
 		$posts = get_posts( array(
-			'post_type' => $custom_post_type,
+			'post_type' => $this->loyalty_program_post_type,
 			'post_status' => 'publish',
 			'posts_per_page' => -1,
 		) );
 	
-		$count = count( $posts );
-	
-		for ( $i = 1; $i <= $count; $i++ ) {
-			$field_name = 'custom_field_' . $i;
-			$field_label = 'Custom Field ' . $i;
-			$field_id = 'custom_field_' . $i;
+		foreach($posts as $post) {
+			$field_name = $post->post_name;
+			$field_label = $post->post_title;
+			$field_id =  $post->post_name;
 			$field_required = false; // Можете изменить на false, если поле не обязательное
 	
 			woocommerce_form_field( $field_name, array(
-				'type' => 'text',
+				'type' => 'date',
 				'class' => array( 'form-row-wide' ),
 				'label' => $field_label,
 				'required' => $field_required,
 				'id' => $field_id,
 			) );
+			
 		}
 	}
 
+	public function save_gate_fields_on_registration( $customer_id ) {    
+		$posts = get_posts( array(
+			'post_type' =>  $this->loyalty_program_post_type,
+			'post_status' => 'publish',
+			'posts_per_page' => -1,
+		) );
+		$customer_data = get_userdata( $customer_id );
+		$customer_email = $customer_data->user_email;
+
+		foreach($posts as $post) {
+			$field_name = $post->post_name;
+			if ( isset( $_POST[ $field_name ] ) and !empty(  $_POST[ $field_name ] ) ) {
+				$date_celeb = $_POST[ $field_name ];
+				$meta_key = $field_name;
+				$meta_value = sanitize_text_field( $_POST[ $field_name ] );
+	
+				update_user_meta( $customer_id, $meta_key, $meta_value );
+				
+				$amount = get_post_meta($post->ID, 'discount_amount_key', true);
+				if(empty($amount)) {
+					$amount = "5";
+				}
+				$discount_type = 'percent';
+				$coupon_code = 'DATE' . $customer_id . '&' . $post->ID;
+
+				$coupon = array(
+					'post_title' => $coupon_code,
+					'post_content' => '',
+					'post_status' => 'publish',
+					'post_author' => 1,
+					'post_type' => 'shop_coupon' );
+					
+				$new_coupon_id = wp_insert_post( $coupon );
+
+				$date_exp = strtotime($date_celeb . "+1 days");
+
+				if ( $new_coupon_id ) {
+
+					update_post_meta( $new_coupon_id, 'discount_type', $discount_type );
+					update_post_meta( $new_coupon_id, 'coupon_amount', $amount );
+					update_post_meta( $new_coupon_id, 'individual_use', 'yes' );
+										
+					update_post_meta( $new_coupon_id, 'exclude_product_ids', '' );
+					update_post_meta( $new_coupon_id, 'usage_limit', '1' );
+					update_post_meta( $new_coupon_id, 'expiry_date', '' );
+					update_post_meta( $new_coupon_id, 'apply_before_tax', 'yes' );
+					update_post_meta( $new_coupon_id, 'free_shipping', 'no' );
+					update_post_meta( $new_coupon_id, 'customer_email', array($customer_email) );
+
+					update_post_meta( $new_coupon_id, 'date_expires', $date_exp );
+
+					$tomorrow = date('m-d-Y',strtotime($date1 . "+1 days"));
+				
+				} else {
+				
+					update_user_meta( $customer_id, '_coupon_error', 'create error' );
+				}
+			}
+		}
+	}
 
 
 }
