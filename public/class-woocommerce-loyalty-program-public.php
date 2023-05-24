5c81f733d6b49e4233a44b32122630c4ec7674b0
Роман Ченский <romaleg.sky@yandex.ru>
2023-05-24 18:26:56 +0300

    added

--- public/class-woocommerce-loyalty-program-public.php
+++ public/class-woocommerce-loyalty-program-public.php
@@ -53,6 +53,18 @@ class Woocommerce_Loyalty_Program_Public {
 		$this->version = $version;
 		add_action( 'woocommerce_register_form', [ $this, 'add_date_fields_to_registration_form' ] );
 		add_filter( 'woocommerce_created_customer', [ $this, 'save_gate_fields_on_registration' ], 10, 1 );
+		add_filter ( 'woocommerce_account_menu_items', [ $this, 'add_personal_account_page'], 25 );
+		add_action( 'init', [ $this, 'memorable_dates_add_endpoint'], 25 );
+		add_action( 'woocommerce_account_memorable-dates_endpoint', [ $this, 'memorable_dates_content'], 25 );
+
+		add_action( 'wp_ajax_add_new_date', 'ajax_add_new_date' ); 
+		add_action( 'wp_ajax_nopriv_add_new_date', 'ajax_add_new_date' );
+
+		add_action( 'wp_ajax_remove_date', 'ajax_remove_date' ); 
+		add_action( 'wp_ajax_nopriv_remove_date', 'ajax_remove_date' );
+
+		add_action( 'wp_ajax_change_date', 'ajax_change_date' ); 
+		add_action( 'wp_ajax_nopriv_change_date', 'ajax_change_date' );
 	}
 
 	/**
@@ -111,8 +123,6 @@ class Woocommerce_Loyalty_Program_Public {
 			'post_status' => 'publish',
 			'posts_per_page' => -1,
 		) );
-
-
 		?>
 			<a href="#add_notification_dates" data-open="#add_notification_dates" class="primary is-small button wp-element-button is-outline"><?php echo __( 'Add date', 'woocommerce-loyalty-program' );?></a>
 
@@ -203,10 +213,11 @@ class Woocommerce_Loyalty_Program_Public {
 		$test = json_encode($_POST);
 		update_user_meta( $customer_id, 'test', $test );
 		
-		$notification_flag = $_POST[ 'notification_flag' ];
-		update_user_meta( $customer_id, 'notification_flag', $notification_flag );
-
-
+		$notification_flag = false;
+		if(isset($_POST[ 'notification_flag' ])) {
+			update_user_meta( $customer_id, 'notification_flag', true );
+			$notification_flag = true;
+		}
 
 		$customer_data = get_userdata( $customer_id );
 		$customer_email = $customer_data->user_email;
@@ -215,11 +226,10 @@ class Woocommerce_Loyalty_Program_Public {
 		$last_name = get_usermeta($customer_id, 'last_name', true);
 		$name = trim($first_name . ' ' . $last_name);
 
-		$notify_dates = $_POST['notify_dates'];
-		$notify_dates_names = $_POST['notify_dates_name'];
-
 		$data_array = array("email" => $customer_email);
 		$data_array['variables'] = array();
+
+		$data_array['variables']['language'] = get_locale();
 
 		if(!empty($customer_phone)){
 			$data_array['variables']['Phone'] = $customer_phone;
@@ -228,103 +238,228 @@ class Woocommerce_Loyalty_Program_Public {
 			$data_array['variables']['Имя'] = $name;
 		}
 
-		foreach($notify_dates as $key => $value) {
-			$name_celebrate = $notify_dates_names[$key];
-			$all_string = $name_celebrate . $value;
-			$key_hash = substr(sha1($all_string), 0, 10);
-			// update_user_meta( $customer_id, 'test_key_hash' . $key_hash,  );
-			$discount_type = 'percent';
-			$coupon_code = 'DT' . $customer_id . 'EE' . strtolower($key_hash);
+		if (isset($_POST['notify_dates']) and !empty($_POST['notify_dates']) ) {
+			$notify_dates = $_POST['notify_dates'];
+			$notify_dates_names = $_POST['notify_dates_name'];
 
-			$date_arr = explode('||', $value);
-			$date_celeb = $date_arr[1];
-
-			update_user_meta( $customer_id, 'test_name_date' . $key, $date_arr[0] );
-			update_user_meta( $customer_id, 'test_date_date' . $key, $date_arr[1] );
-
-			update_user_meta( $customer_id, 'test_name_celebrate' . $key, $name_celebrate );
-			
-			$coupon = array(
-				'post_title' => $coupon_code,
-				'post_content' => '',
-				'post_status' => 'publish',
-				'post_author' => 1,
-				'post_type' => 'shop_coupon' );
-
-			$date_exp = strtotime($date_celeb . "+1 days");
-			$date_start = strtotime($date_celeb . "-6 days");
-
-			update_user_meta( $customer_id, '_date_exp_' . $date_arr[0], $date_exp );
-			update_user_meta( $customer_id, '_date_strt_'  . $date_arr[0], $date_start );
-
-			$args = array(
-				'name'        => $date_arr[0],
-				'post_type'   => 'loyalty_program',
-				'post_status' => 'publish',
-				'numberposts' => 1
-			);
-
-			$current_celebrate = get_posts($args);
-			update_user_meta( $customer_id, 'test_test' . $key, json_encode($current_celebrate) );
-
-
-			$date_for_sendpulse_array = explode('.', $date_arr[1]);
-			$date_for_sendpulse = $date_for_sendpulse_array[1] . '/' .$date_for_sendpulse_array[0] . '/' . $date_for_sendpulse_array[2];
-			$data_array['variables'][$date_arr[0]] = $date_for_sendpulse;
-
-			if($name_celebrate != '- -') {
-				$data_array['variables']['name_'.$date_arr[0]] = $name_celebrate;
-			}
-
-			if( $current_celebrate ) {
-				$new_coupon_id = wp_insert_post( $coupon );
-
-				$amount = get_post_meta($current_celebrate[0]->ID, 'discount_amount_key', true);
-				if(empty($amount)) {
-					$amount = "10";
-				}
-
-				if ( $new_coupon_id ) {
-
-					update_post_meta( $new_coupon_id, 'discount_type', $discount_type );
-					update_post_meta( $new_coupon_id, 'coupon_amount', $amount );
-					update_post_meta( $new_coupon_id, 'individual_use', 'yes' );
-										
-					update_post_meta( $new_coupon_id, 'exclude_product_ids', '' );
-					update_post_meta( $new_coupon_id, 'usage_limit', '1' );
-					update_post_meta( $new_coupon_id, 'expiry_date', '' );
-					update_post_meta( $new_coupon_id, 'apply_before_tax', 'yes' );
-					update_post_meta( $new_coupon_id, 'free_shipping', 'no' );
-					update_post_meta( $new_coupon_id, 'customer_email', array($customer_email) );
+			foreach($notify_dates as $key => $value) {
+				$name_celebrate = $notify_dates_names[$key];
+				$all_string = $name_celebrate . $value;
+				$key_hash = substr(sha1($all_string), 0, 10);
+				// update_user_meta( $customer_id, 'test_key_hash' . $key_hash,  );
+				$discount_type = 'percent';
+				$coupon_code = 'DT' . $customer_id . 'EE' . strtolower($key_hash);
 	
-					update_post_meta( $new_coupon_id, 'date_expires', $date_exp );
-					update_post_meta( $new_coupon_id, '_wt_coupon_start_date', date('Y-m-d', $date_start) );
+				$date_arr = explode('||', $value);
+				$date_celeb = $date_arr[1];
 	
-					update_post_meta( $new_coupon_id, '_user_id', $customer_id );
-
-					update_user_meta( $customer_id, 'coupon_for_' . $date_arr[0], $coupon_code );
-					
-					$data_array['variables']['coupon_' . $date_arr[0]] = $coupon_code;
-				} else {
+				update_user_meta( $customer_id, 'test_name_date' . $key, $date_arr[0] );
+				update_user_meta( $customer_id, 'test_date_date' . $key, $date_arr[1] );
+	
+				update_user_meta( $customer_id, 'test_name_celebrate' . $key, $name_celebrate );
 				
-					update_user_meta( $customer_id, '_coupon_error', 'create error' );
-				}
-				
+				$coupon = array(
+					'post_title' => $coupon_code,
+					'post_content' => '',
+					'post_status' => 'publish',
+					'post_author' => 1,
+					'post_type' => 'shop_coupon' );
 	
-
-			} else {
-				update_user_meta( $customer_id, '_coupon_error', 'celebration date notfound' . $date_arr[0] );
-			}
-
-		}
-
-		if($notification_flag) {
-			require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-woocommerce-loyalty-program-sendpulse-api.php';
-			$sendPulse = new SendPulseApi;
-			$sendPulse->add_new_and_change_address($data_array, $customer_id);
-		}
+				$date_exp = strtotime($date_celeb . "+1 days");
+				$date_start = strtotime($date_celeb . "-6 days");
+	
+				update_user_meta( $customer_id, '_date_exp_' . $date_arr[0], $date_exp );
+				update_user_meta( $customer_id, '_date_strt_'  . $date_arr[0], $date_start );
+	
+				$args = array(
+					'name'        => $date_arr[0],
+					'post_type'   => 'loyalty_program',
+					'post_status' => 'publish',
+					'numberposts' => 1
+				);
+	
+				$current_celebrate = get_posts($args);
+				update_user_meta( $customer_id, 'test_test' . $key, json_encode($current_celebrate) );
+	
+	
+				$date_for_sendpulse_array = explode('.', $date_arr[1]);
+				$date_for_sendpulse = $date_for_sendpulse_array[1] . '/' .$date_for_sendpulse_array[0] . '/' . $date_for_sendpulse_array[2];
+				$data_array['variables'][$date_arr[0]] = $date_for_sendpulse;
+	
+				if($name_celebrate != '- -') {
+					$data_array['variables']['name_'.$date_arr[0]] = $name_celebrate;
+				}
+	
+				if( $current_celebrate ) {
+					$new_coupon_id = wp_insert_post( $coupon );
+	
+					$amount = get_post_meta($current_celebrate[0]->ID, 'discount_amount_key', true);
+					if(empty($amount)) {
+						$amount = "10";
+					}
+	
+					if ( $new_coupon_id ) {
+	
+						update_post_meta( $new_coupon_id, 'discount_type', $discount_type );
+						update_post_meta( $new_coupon_id, 'coupon_amount', $amount );
+						update_post_meta( $new_coupon_id, 'individual_use', 'yes' );
+											
+						update_post_meta( $new_coupon_id, 'exclude_product_ids', '' );
+						update_post_meta( $new_coupon_id, 'usage_limit', '1' );
+						update_post_meta( $new_coupon_id, 'expiry_date', '' );
+						update_post_meta( $new_coupon_id, 'apply_before_tax', 'yes' );
+						update_post_meta( $new_coupon_id, 'free_shipping', 'no' );
+						update_post_meta( $new_coupon_id, 'customer_email', array($customer_email) );
 		
+						update_post_meta( $new_coupon_id, 'date_expires', $date_exp );
+						update_post_meta( $new_coupon_id, '_wt_coupon_start_date', date('Y-m-d', $date_start) );
+		
+						update_post_meta( $new_coupon_id, '_user_id', $customer_id );
+	
+						update_user_meta( $customer_id, 'coupon_for_' . $date_arr[0], $coupon_code );
+						
+						$data_array['variables']['coupon_' . $date_arr[0]] = $coupon_code;
+					} else {
+					
+						update_user_meta( $customer_id, '_coupon_error', 'create error' );
+					}
+					
+		
+	
+				} else {
+					update_user_meta( $customer_id, '_coupon_error', 'celebration date notfound' . $date_arr[0] );
+				}
+	
+			}
+	
+			if($notification_flag) {
+				require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-woocommerce-loyalty-program-sendpulse-api.php';
+				$sendPulse = new SendPulseApi;
+				$sendPulse->add_new_and_change_address($data_array, $customer_id);
+			}
+		}
+	}
 
+	public function add_personal_account_page($menu_links) {
+		$menu_links[ 'memorable-dates' ] = __('Memorable dates', 'woocommerce-loyalty-program');
+		return $menu_links;
+	}
+
+	public function memorable_dates_add_endpoint() {
+	
+		add_rewrite_endpoint( 'memorable-dates', EP_PAGES );
+	
+	}
+	
+	public function memorable_dates_content() {
+		$user_id = get_current_user_id();
+		$posts = get_posts( array(
+			'post_type' => $this->loyalty_program_post_type,
+			'post_status' => 'publish',
+			'posts_per_page' => -1,
+		) );
+		?>
+		<div class="personal_account_dates_wrapper">
+			<?php
+				for($i=0; $i <=100;  $i++) {
+					if (get_user_meta($user_id, 'test_date_date' . $i, true)) {
+						$name_celeb = get_user_meta($user_id, 'test_name_celebrate' . $i, true);
+						$generated_slug = get_user_meta($user_id, 'test_name_date' . $i, true);
+						$pattern = '/\s+/';
+						$name_clean = preg_replace($pattern, '', $name_celeb);
+						$generated_slug .= $name_clean;
+					?>
+						<div class="item <?php echo $generated_slug; ?>">
+							<div class="name"><?php echo $name_celeb; ?></div>
+							<div class="date">
+								<?php
+									$date = get_user_meta($user_id, 'test_date_date' . $i, true);
+									$celeb_date = strtotime($date);
+									$newformat_date = date("j, F", $celeb_date);
+									echo $newformat_date;
+								?>
+							</div>
+							<div class="change-delete">
+								<a href="#" class="change_date_pa">Change</a>
+								<a href="#" class="delete_date_pa"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></a>
+							</div>
+						</div>
+					<?php
+					} else {
+						break;
+					}
+				}
+			?>
+			<a href="#add_notification_dates" data-open="#add_notification_dates" class="primary is-small button wp-element-button is-outline"><?php echo __('Add new date', 'woocommerce-loyalty-program');?> +</a>
+		</div>
+
+		<div id="add_notification_dates" class="dark text-center mfp-hide lightbox-content">
+			<div class="account-container lightbox-inner">
+				<div class="row row-divided row-large" id="add_user_date">
+					<div class="col-1 large-12 col pb-0">
+						<h3 class="uppercase"><?php echo __( 'Add date notification', 'woocommerce-loyalty-program' );?></h3>
+						<p class="form-row form-row-first">
+							<label for="add_date_new_firs_name"><?php echo __( 'First Name', 'woocommerce-loyalty-program' );?></label>
+							<input type="text" class="input-text" name="add_date_new_firs_name" id="add_date_new_firs_name" value="">
+						</p>
+						<p class="form-row form-row-last">
+							<label for="add_date_new_firs_name"><?php echo __( 'Last Name', 'woocommerce-loyalty-program' );?></label>
+							<input type="text" class="input-text" name="add_date_new_last_name" id="add_date_new_last_name" value="">
+						</p>
+
+						<p class="add_notification_date--info">
+							<?php echo __( 'Mark the events you want to be notified about. You will receive an email notification a few days before the event. By mail with the offer of the desired package.', 'woocommerce-loyalty-program' );?>
+						</p>
+
+						<?php if(!empty($posts)) { ?>
+							<div class="notification_dates-wrapper">
+							<?php 
+								foreach($posts as $date): 
+								$id = $date->ID;
+								$celeb_date = get_post_meta($id, '_date_discount', true);
+								if(!empty($celeb_date)) {
+									$celeb_date = strtotime($celeb_date);
+									$newformat_date = date("j, F", $celeb_date);
+									$newformat_date_value = date("d.m.Y", $celeb_date);
+								}
+								
+							?>
+								<div class="notification_dates-item">
+									<input type="checkbox" name="date_notify" value="<?php echo $date->post_name; ?>">
+									<span><?php echo $date->post_title; ?></span>
+									<?php if(!empty($celeb_date)): ?>
+									<div class="date-wrapper">
+										<?php echo $newformat_date; ?>
+										<input type="hidden" name="" class="input-date_with_datepicker" value="<?php echo $newformat_date_value; ?>" id="">
+									</div>
+									<?php else: ?>
+									<div class="date-wrapper datePicker_wrapper chose_date">
+										<input type="text" class="woocommerce-Input woocommerce-Input--text input-date_with_datepicker input-text" placeholder="<?php echo __( 'Choose date', 'woocommerce-loyalty-program' );?>" name="chosen_date_<?php echo $date->post_name; ?>" id="chosen_date_<?php echo $date->post_name; ?>" autocomplete="off">
+									</div>
+									<?php endif; ?>
+									
+								</div>
+							<?php endforeach; ?>
+							<a href="#" id="add_new_date_notification_pk" class="woocommerce-button button"><?php echo __( 'Add', 'woocommerce-loyalty-program' );?></a>
+							</div>
+						<?php } ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	<?php
+	}
+
+	public function ajax_add_new_date() {
+
+	}
+
+	public function ajax_remove_date() {
+		
+	}
+
+	public function ajax_change_date() {
+		
 	}
 
 }
--- public/js/woocommerce-loyalty-program-public.js
+++ public/js/woocommerce-loyalty-program-public.js
@@ -78,6 +78,7 @@
 
 				let html = `<div class="item_added_dates ${generated_class}"><span class="name">${name_date} ${last_name_date}</span><span class="date">${visible_date}</span><input type="hidden" value="${date_slug}||${date}" name="notify_dates[]"><input type="hidden" name="notify_dates_name[]" value="${name_date} ${last_name_date}"><span class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span></div>`
 				button.before(html)
+				$('#add_notification_dates').magnificPopup('close')
 			} else {
 				$(this).find('.input-date_with_datepicker').removeClass('empty')
 			}
@@ -92,13 +93,87 @@
 				$(this).val('')
 			}
 		})
-		$('#add_notification_dates').magnificPopup('close')
 
 		$('.item_added_dates .delete').on('click', function() {
 			$(this).parent().remove()
 		})
 
 		return false;
+	})
+
+	$('#add_new_date_notification_pk').on('click', function() {
+		let date_arr = {}
+		const name_date = $('#add_date_new_firs_name').val() || '-'
+		const last_name_date = $('#add_date_new_last_name').val() || '-'
+		const button = $('a[href="#add_notification_dates"]')
+		let date
+		let date_slug
+
+
+		date_arr['name'] = name_date
+		date_arr['last_name'] = last_name_date
+
+		
+		$('.notification_dates-item').each(function() {
+			if($(this).find('input[type="checkbox"]').is(':checked')) {
+				date = $(this).find('.input-date_with_datepicker').val()
+				date_slug = $(this).find('input[type="checkbox"]').val()
+				if(date == '') {
+					$(this).find('.input-date_with_datepicker').addClass('empty')
+					return false
+				} else {
+					date_arr[date_slug] = date
+				}
+
+				let visible_date = date.split('.')
+				visible_date = `${visible_date[0]}.${visible_date[1]}`
+
+				let generated_class = `${date_slug}${name_date.replaceAll(/\s/g,'')}${last_name_date.replaceAll(/\s/g,'')}`
+
+				let html = `<div class="item ${generated_class}"><div class="name">${name_date} ${last_name_date}</div>`
+				html += `<div class="date ">${visible_date}</div>`
+				html += `<div class="change-delete"><a href="#" class="change_date_pa">Change</a>`
+				html += `<a href="#" class="delete_date_pa"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></a>`
+				html += `</div></div>`
+
+				if($(`.${generated_class}`).length) {
+					$(`.${generated_class}`).replaceWith(html)
+				} else {
+					button.before(html)
+				}
+
+				$('#add_notification_dates').magnificPopup('close')
+				$('#add_notification_dates input').each(function() {
+					if($(this).attr('type') == 'checkbox') {
+						$(this).removeAttr("checked");
+						$(this).parent().removeClass('checked')
+					} else if($(this).attr('type') == 'text') {
+						$(this).val('')
+					}
+				})
+
+				let user_id = $('#user_id').val();
+
+				$.ajax({
+					url: '/wp-admin/admin-ajax.php',
+					type: 'POST',
+					data: `user_id=${user_id}&param1=2&param2=3`,
+					success: function( data ) {
+						console.log(data);
+					}
+				});
+
+			} else {
+				$(this).find('.input-date_with_datepicker').removeClass('empty')
+			}
+			
+		})
+
+		return false;
+	})
+
+	$('.personal_account_dates_wrapper .delete_date_pa').on('click', function() {
+		$(this).parent().parent().remove()
 	})
 	
 
