c7330b9f27e0fc2abaad036d24e611e45a3dc1f0
Роман Ченский <romaleg.sky@yandex.ru>
2023-05-25 18:13:39 +0300

    added

--- public/js/woocommerce-loyalty-program-public.js
+++ public/js/woocommerce-loyalty-program-public.js
@@ -72,11 +72,11 @@
 
 				let generated_class = `${date_slug}${name_date.replaceAll(/\s/g,'')}${last_name_date.replaceAll(/\s/g,'')}`
 
-				if($(`.${generated_class}`).length) {
-					$(`.${generated_class}`).remove()
+				if($(`.${date_slug}`).length) {
+					$(`.${date_slug}`).remove()
 				}
 
-				let html = `<div class="item_added_dates ${generated_class}"><span class="name">${name_date} ${last_name_date}</span><span class="date">${visible_date}</span><input type="hidden" value="${date_slug}||${date}" name="notify_dates[]"><input type="hidden" name="notify_dates_name[]" value="${name_date} ${last_name_date}"><span class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span></div>`
+				let html = `<div class="item_added_dates ${date_slug}"><span class="name">${name_date} ${last_name_date}</span><span class="date">${visible_date}</span><input type="hidden" value="${date_slug}||${date}" name="notify_dates[]"><input type="hidden" name="notify_dates_name[]" value="${name_date} ${last_name_date}"><span class="delete"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></span></div>`
 				button.before(html)
 				$('#add_notification_dates').magnificPopup('close')
 			} else {
@@ -130,14 +130,14 @@
 
 				let generated_class = `${date_slug}${name_date.replaceAll(/\s/g,'')}${last_name_date.replaceAll(/\s/g,'')}`
 
-				let html = `<div class="item ${generated_class}"><div class="name">${name_date} ${last_name_date}</div>`
+				let html = `<div class="item ${date_slug}"><div class="name">${name_date} ${last_name_date}</div>`
 				html += `<div class="date ">${visible_date}</div>`
 				html += `<div class="change-delete"><a href="#" class="change_date_pa">Change</a>`
 				html += `<a href="#" class="delete_date_pa"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></a>`
 				html += `</div></div>`
 
-				if($(`.${generated_class}`).length) {
-					$(`.${generated_class}`).replaceWith(html)
+				if($(`.${date_slug}`).length) {
+					$(`.${date_slug}`).replaceWith(html)
 				} else {
 					button.before(html)
 				}
